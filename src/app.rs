e2cb6cb82c806f3f02f71ec71ef483b1675c4558
Harry Coleman <harrymcoleman@gmail.com>
2023-01-25 22:34:49 -0800

    render components using data from Board struct

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,23 @@
+use std::collections::HashSet;
+
 use yew::prelude::*;
+
+use crate::sudoku::board::{Board, Cell, CellContent, Digit, DIGITS};
+
+// use crate::sudoku::{Board, Cell};
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let board = use_state(|| {
+        Board::from_string(
+            "607005010580007900000060000005000009000936000300000400000080000003600094050200806",
+        )
+    });
+
     html! {
         <div class={classes!("app")}>
             <Grid
-                board={"test board".to_string()}
+                board={(*board).clone()}
                 // selectedCells={this.state.selectedCells}
                 // result={this.state.result}
                 // focus={this.state.focus}
@@ -36,16 +48,18 @@ pub fn app() -> Html {
 
 #[derive(Properties, PartialEq)]
 struct GridProps {
-    board: String,
+    board: Board,
 }
 
 #[function_component(Grid)]
 fn grid(props: &GridProps) -> Html {
     let board = &props.board;
 
-    let boxes = (0..9).map(|i| {
+    let boxes = board.boxes().map(|id_box| {
+        let cells = id_box.map(Clone::clone);
+
         html! {
-            <BoxComponent key={i}/>
+            <BoxComponent cells={cells} />
         }
     });
 
@@ -56,11 +70,18 @@ fn grid(props: &GridProps) -> Html {
     }
 }
 
+#[derive(Properties, PartialEq)]
+struct BoxProps {
+    cells: [Cell; 9],
+}
+
 #[function_component(BoxComponent)]
-fn box_component() -> Html {
-    let cells = (0..9).map(|i| {
+fn box_component(props: &BoxProps) -> Html {
+    let cells = props.cells.clone();
+
+    let cells = cells.map(|cell| {
         html! {
-            <CellComponent key={i}/>
+            <CellComponent cell={cell} />
         }
     });
 
@@ -71,13 +92,89 @@ fn box_component() -> Html {
     }
 }
 
+#[derive(Properties, PartialEq)]
+struct CellProps {
+    cell: Cell,
+}
+
 #[function_component(CellComponent)]
-fn cell_component() -> Html {
+fn cell_component(props: &CellProps) -> Html {
+    let content = props.cell.content.clone();
+
+    let content = match content {
+        CellContent::Digit(digit, given) => {
+            html! {
+                <CellDigit digit={digit} given={given} />
+            }
+        }
+        CellContent::Notes(notes) => {
+            html! {
+                <CellNotes notes={notes} />
+            }
+        }
+    };
+
     html! {
         <div class={classes!("cell")}>
-            <div class={classes!("cell-digit")}>
-                { "X" }
-            </div>
+            { content }
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct CellDigitProps {
+    digit: Digit,
+    given: bool,
+}
+
+#[function_component(CellDigit)]
+fn cell_digit(props: &CellDigitProps) -> Html {
+    let digit = props.digit.to_string();
+    let given = props.given.then_some("given");
+
+    html! {
+        <div class={classes!("cell-digit", given)}>
+            { digit }
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct CellNotesProps {
+    notes: HashSet<Digit>,
+}
+
+#[function_component(CellNotes)]
+fn cell_notes(props: &CellNotesProps) -> Html {
+    let notes = props.notes.clone();
+
+    let notes = DIGITS.map(|digit| {
+        html! {
+            <Note digit={digit} is_shown={notes.contains(&digit)} />
+        }
+    });
+
+    html! {
+        <div class={classes!("cell-notes")}>
+            { for notes }
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct NoteProps {
+    digit: Digit,
+    is_shown: bool,
+}
+
+#[function_component(Note)]
+fn note(props: &NoteProps) -> Html {
+    let digit = props.digit.to_string();
+    let shown = props.is_shown.then_some("shown");
+
+    html! {
+        <div class={classes!("note", shown)}>
+            { digit }
         </div>
     }
 }
---
+++ src/sudoku/board/mod.rs
@@ -0,0 +1,83 @@
+use std::{
+    cell,
+    collections::{HashMap, HashSet},
+};
+
+mod id;
+pub use id::*;
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub enum CellContent {
+    Digit(Digit, bool),
+    Notes(HashSet<Digit>),
+}
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct Cell {
+    pub id: CellId,
+    pub content: CellContent,
+}
+
+impl Cell {
+    fn new(id: CellId) -> Cell {
+        let notes = HashSet::from_iter(DIGITS);
+
+        Cell {
+            id,
+            content: CellContent::Notes(notes),
+        }
+    }
+
+    fn new_given(id: CellId, digit: Digit) -> Cell {
+        Cell {
+            id,
+            content: CellContent::Digit(digit, true),
+        }
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct Board {
+    cells: HashMap<CellId, Cell>,
+}
+
+impl Board {
+    pub fn new() -> Board {
+        let mut cells = HashMap::new();
+
+        for id in CELLS {
+            cells.insert(id, Cell::new(id));
+        }
+
+        Board { cells }
+    }
+
+    pub fn from_string(string: &str) -> Board {
+        let mut cells = HashMap::new();
+
+        for (i, &id) in CELLS.iter().enumerate() {
+            let digit = string.chars().nth(i).and_then(|c| Digit::try_from(c).ok());
+
+            let cell = match digit {
+                Some(digit) => Cell::new_given(id, digit),
+                None => Cell::new(id),
+            };
+
+            cells.insert(id, cell);
+        }
+
+        Board { cells }
+    }
+
+    pub fn get_cell(&self, id: CellId) -> &Cell {
+        self.cells.get(&id).unwrap()
+    }
+
+    pub fn cells(&self) -> [&Cell; 81] {
+        CELLS.map(|id| self.get_cell(id))
+    }
+
+    pub fn boxes(&self) -> [[&Cell; 9]; 9] {
+        BOXES.map(|id_box| id_box.map(|id| self.get_cell(id)))
+    }
+}
